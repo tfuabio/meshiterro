2d2ab5b48d3856b17eb2e3ab787a26ba0cbc701f
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-11 11:04:20 +0000

    [update] 3-9 ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
+  resources :post_images, only: [:new, :index, :show]
 end
